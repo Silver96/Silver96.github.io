c290b5e18e409aa03c2bc23b13328de39904e4fd
Silas <96andrea9642@gmail.com>
2020-09-27 20:22:46 +0200

    gh-pages

---
+++ precache-manifest.0508d7096e17d214a60ee40a6ae68884.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39b13cdb940be9c94a4747384740a023",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e0de065cce65813065d",
+    "url": "/static/css/main.64920e63.chunk.css"
+  },
+  {
+    "revision": "ebce666df60faeb0273d",
+    "url": "/static/js/2.d7c3d510.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d7c3d510.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e0de065cce65813065d",
+    "url": "/static/js/main.08fe26fe.chunk.js"
+  },
+  {
+    "revision": "42db428a609ae223099f",
+    "url": "/static/js/runtime-main.6c8a60c2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
